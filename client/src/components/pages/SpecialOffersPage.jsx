5cc83a430f0a09db27726ebea8cb4ab976d47ca5
hojongyu2 <yuhojo91@gmail.com>
2023-06-21 00:11:19 -0400

    Updated sized of the background picture in SpecialOffersPage

--- client/src/components/pages/SpecialOffersPage.jsx
+++ client/src/components/pages/SpecialOffersPage.jsx
@@ -5,28 +5,32 @@ import { useNavigate } from "react-router-dom"
 
 export const SpecialOffersPage = () => {
     const navivate = useNavigate()
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         setTimeout(() => {
             navivate('/')
         }, 2000);
-    },[])
+    }, [])
 
     return (
         <Layout>
             <Box sx={{
-        width:'100vw',
-        height:'80vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundImage:
-        "linear-gradient(rgb(60, 60, 60), rgba(0, 0, 0, 0.1)), url('https://media.istockphoto.com/id/1332167985/photo/coming-soon-neon-sign-the-banner-shining-light-signboard-collection.jpg?b=1&s=170667a&w=0&k=20&c=Le4B-lJt-jXjeAOdlTQptNvN_DmRwWF19ShNc5VY4a4=')",
-      }}>
+                width: '100vw',
+                height: '80vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage:
+                "linear-gradient(rgb(60, 60, 60), rgba(0, 0, 0, 0.1)), url('https://media.istockphoto.com/id/1332167985/photo/coming-soon-neon-sign-the-banner-shining-light-signboard-collection.jpg?b=1&s=170667a&w=0&k=20&c=Le4B-lJt-jXjeAOdlTQptNvN_DmRwWF19ShNc5VY4a4=')",
+            '@media (max-width: 600px)': { // Adjust the breakpoint value as needed
+                height: '50vh', // Adjust the height for mobile view
+                backgroundSize: 'contain', // Modify the background size to fit within the box
+            },
+            }}>
             </Box>
         </Layout>
     )
